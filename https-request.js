4b3969ba5629d0cba1217299b6f22fff6360ad58
grantran <grant.tran@gmail.com>
2017-05-08 18:31:09 +0000

    wrote getAndPrintHTMLChunks function

--- https-request.js
+++ https-request.js
@@ -1,1 +1,25 @@
-## second commit
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log('Chunk Receieved. Data: ', data)
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  })
+
+}
+
+getAndPrintHTMLChunks();
